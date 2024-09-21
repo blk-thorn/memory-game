63512f5807fcb055d18970b1c65b740cb7dbbf62
Victoria Ternovaya <victoria.ternovaya@gmail.com>
2024-09-21 21:33:57 +0300

    docs: add assets and songs array

---
+++ audio-player/data.js
@@ -0,0 +1,32 @@
+export const songs = [
+    {
+        name: 'song 1',
+        path: 'assets/audio/Song 1.mp3',
+        artist: 'artist 1',
+        cover: 'assets/images/cover1.png'
+    },
+    {
+        name: 'song 2',
+        path: 'assets/audio/Song 2.mp3',
+        artist: 'artist 2',
+        cover: 'assets/images/cover2.png'
+    },
+    {
+        name: 'song 3',
+        path: 'assets/audio/Song 3.mp3',
+        artist: 'artist 3',
+        cover: 'assets/images/cover3.png'
+    },
+    {
+        name: 'song 4',
+        path: 'assets/audio/Song 4.mp3',
+        artist: 'artist 4',
+        cover: 'assets/images/cover4.png'
+    },
+    {
+        name: 'song 5',
+        path: 'assets/audio/Song 5.mp3',
+        artist: 'artist 5',
+        cover: 'assets/images/cover5.png'
+    },
+]
