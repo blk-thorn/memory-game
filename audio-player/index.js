103170b3b1016126fa95baa4a100cb5ae9ee5b90
Victoria Ternovaya <victoria.ternovaya@gmail.com>
2024-09-21 22:20:49 +0300

    feat: add control functions

--- audio-player/index.js
+++ audio-player/index.js
@@ -23,3 +23,49 @@ function loadMusic(index) {
     trackAudio.src = songs[index].path;
 
 }
+
+
+const playPauseButton = wrapper.querySelector('button[title="Play/Pause"]');
+const progressBar = wrapper.querySelector(".progress-bar");
+const progress = wrapper.querySelector(".progress");
+// const playIcon = document.getElementById("play-plist");
+// const pauseIcon = document.getElementById("pause-plist");
+
+const prevButton = wrapper.querySelector(".prev__btn");
+const nextButton = wrapper.querySelector(".next__btn");
+const loopButton = wrapper.querySelector(".loop__btn i");
+const shuffleButton = wrapper.querySelector(".shuffle__btn");
+
+
+//play  music function 
+function playMusic() {
+    wrapper.classList.add("paused");
+    playPauseButton.querySelector("i").innerText = "pause"
+    trackAudio.play();
+}
+
+//pause  music function 
+function pauseMusic() {
+    wrapper.classList.remove("paused");
+    playPauseButton.querySelector("i").innerText = "play_arrow"
+    trackAudio.pause();
+}
+
+function nextTrack() {
+    trackIndex++;
+    if (trackIndex >= songs.length) {
+        trackIndex = 0;
+    };
+    loadMusic(trackIndex);
+    playMusic();
+}
+
+function prevTrack() {
+    trackIndex--;
+    if (trackIndex < 0) {
+        trackIndex = [songs.length - 1];
+    };
+    loadMusic(trackIndex);
+    playMusic();
+}
+
