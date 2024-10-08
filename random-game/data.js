64bf80befd6c5f75f10e2d09ebc0cfdaf71b2808
Victoria Ternovaya <victoria.ternovaya@gmail.com>
2024-10-08 17:21:58 +0300

    docs: add card images and data.js

---
+++ random-game/data.js
@@ -0,0 +1,20 @@
+export const cards = [
+    {
+        img: './img/twin_peaks_icons_1.png'
+    },
+    {
+        img: './img/twin_peaks_icons_2.png'
+    },
+    {
+        img: './img/twin_peaks_icons_3.png'
+    },
+    {
+        img: './img/twin_peaks_icons_4.png'
+    },
+    {
+        img: './img/twin_peaks_icons_5.png'
+    },
+    {
+        img: './img/twin_peaks_icons_6.png'
+    }   
+]
